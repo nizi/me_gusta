534bca93fc4e1f537b5bc6406959da7d1ff40259
Demetrio Cruz <hesenrre@gmail.com>
2011-07-04 22:39:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeGusta::Application.config.secret_token = '180ad18b8c30bd17457cadd240dedabfdf906134073381455b762b62432ab9c728c520bd2814a170eb32840c5d7a25e3722397c0c78170ff2a4b4e891593f03c'
